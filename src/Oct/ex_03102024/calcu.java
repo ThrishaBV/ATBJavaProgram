d162fbaf3e5671b5d199897ff08bdb9b872019fd
ThrishaBV <thrishabv@gmail.com>
2024-10-04 07:01:05 +0530

    All programs

---
+++ src/Oct/ex_03102024/calcu.java
@@ -0,0 +1,11 @@
+package Oct.ex_03102024;
+
+public class calcu {
+    public static void main(String[] args) {
+        System.out.println(10+5);
+        System.out.println(2*5);
+        System.out.println(30-5);
+
+    }
+
+}
