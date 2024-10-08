07490f86e7b7377da3008ea37e13a9e7c9be2379
ThrishaBV <thrishabv@gmail.com>
2024-10-08 06:57:37 +0530

    08_Oct_datatypes

---
+++ src/Oct/ex_08102024/lab004.java
@@ -0,0 +1,20 @@
+package Oct.ex_08102024;
+
+public class lab004 {
+    public static void main(String[] args) {
+        int age = 65;
+        age = age + 5;
+        System.out.println(age);
+        boolean is_pramod_married =true;
+        System.out.println(is_pramod_married);
+        boolean is_pramod_have_audi =false;
+        System.out.println(is_pramod_have_audi);
+        byte a=120 ;// byte-> -128 to 127 //1byte
+       System.out.println(a);
+        int b= 12456;// 2bytes
+        long c=98765421554L;
+        System.out.println(b);
+        System.out.println(c);
+        float s=3.14f;
+    }
+}
