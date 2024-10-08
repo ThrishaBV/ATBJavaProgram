65c97c7a7da0523d5370f8896165af4ded7fb05a
ThrishaBV <thrishabv@gmail.com>
2024-10-08 06:59:34 +0530

    08_Oct_datatypes

--- src/Oct/ex_03102024/calcu.java
+++ src/Oct/ex_03102024/calcu.java
@@ -1,10 +1,16 @@
+
 package Oct.ex_03102024;
 
 public class calcu {
     public static void main(String[] args) {
+        System.out.println("Addition");
         System.out.println(10+5);
+        System.out.println("Multiplication");
         System.out.println(2*5);
+        System.out.println("Substraction");
         System.out.println(30-5);
+        System.out.println("Division");
+        System.out.println(10/5);
 
     }
 
