f2f60dab1502867d16b93a0606bbe56eb9b60007
ThrishaBV <thrishabv@gmail.com>
2024-10-09 06:57:11 +0530

    09 oct

---
+++ src/Oct/ex_09102024/lab005.java
@@ -0,0 +1,16 @@
+package Oct.ex_09102024;
+
+public class lab005 {
+    //int instance_vari= 20;
+    public static void main(String[] args){
+        char c='%';
+        System.out.println(c);
+        char a='\n';
+        System.out.println(a);
+       // System.out.println(instance_vari);
+        System.out.print("Hello world");
+        System.out.println("Hello world");
+        int age=10;
+        System.out.printf("age is %d",age);
+    }
+}
