0db0797e1b25d9224bb30320851b39b2a9e7b8f1
ThrishaBV <thrishabv@gmail.com>
2024-10-09 20:06:16 +0530

    Task Constants 09_10_2024

---
+++ src/Oct/ex_09102024/constants_task.java
@@ -0,0 +1,8 @@
+package Oct.ex_09102024;
+
+public class constants_task {
+   public static void main(String[] args){
+        final int NUM_PEOPLE=100;
+       System.out.printf("Number of people %d", NUM_PEOPLE);
+   }
+}
