d3cedf7a9c81a6b48e7add68a526af4b625ae428
ThrishaBV <thrishabv@gmail.com>
2024-10-10 19:15:41 +0530

    Task literals 10_10_2024

--- src/Oct/ex_09102024/constants_task.java
+++ src/Oct/ex_09102024/constants_task.java
@@ -4,5 +4,11 @@ public class constants_task {
    public static void main(String[] args){
         final int NUM_PEOPLE=100;
        System.out.printf("Number of people %d", NUM_PEOPLE);
-   }
+       final String STR="abc";
+       System.out.println(STR);
+       final double D=99.9;
+       char new_line ='\n';
+       char a='@';
+       System.out.printf("Thrisha"+ new_line + "ahhs");
+}
 }
---
+++ src/Oct/literals.java
@@ -0,0 +1,24 @@
+package Oct;
+
+public class literals {
+    public static void main(String[] args){
+        //integer literals
+        int x=10;
+        System.out.println(x);
+        //floating-point literals
+        float f=20.45f;
+        System.out.println(f);
+        //character literals
+        char c='A';
+        System.out.println(c);
+        //String literals
+        String str="java";
+        System.out.println(str);
+        //Boolean literals
+        Boolean b=true;
+        System.out.println(b);
+        //null literal
+        String str2=null;
+        System.out.println(str2);
+    }
+}
